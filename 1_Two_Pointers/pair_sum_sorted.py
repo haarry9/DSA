400d899bd1fee3f1881885496098d4f846dc3ebe
haarry9 <hspv99@gmail.com>
2025-06-14 11:16:50 +0000

    pair_sum_sorted

---
+++ 1_Two_Pointers/pair_sum_sorted.py
@@ -0,0 +1,30 @@
+def pair_sum_sorted_brute_force(arr, target):
+
+    for i in range(len(arr)):
+        for j in range(i+1, len(arr)):
+            if (arr[i] + arr[j] == target):
+                return [i, j]
+    
+    return []
+
+# Optimial solution using two pointers
+def pair_sum_sorted(arr, target):
+    l = 0
+    r = len(arr)-1
+    while (l<r):
+        sum = arr[l] + arr[r]
+        if (sum < target):
+            l +=1
+        elif (sum > target):
+            r -= 1
+        else:
+            return [l,r]
+    
+    return []
+
+
+
+
+arr = [-5,-2,3,4,6]
+print(pair_sum_sorted_brute_force(arr,7))
+print(pair_sum_sorted(arr,7))
